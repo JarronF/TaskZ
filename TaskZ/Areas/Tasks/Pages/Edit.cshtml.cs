c53d0ca2b66c2e83d5ca2daa4b321b7797bab534
Jarron Futter <jarronfutter@gmail.com>
2021-02-23 11:11:10 +0200

    Tidy up edit page

--- TaskZ/Areas/Tasks/Pages/Edit.cshtml.cs
+++ TaskZ/Areas/Tasks/Pages/Edit.cshtml.cs
@@ -9,9 +9,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using TaskZ.Models;
 using TaskZ.Data;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TaskZ.Areas.Tasks.Pages
 {
+    [AllowAnonymous]
     public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
